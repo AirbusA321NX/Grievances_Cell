2b4f2bbff0b8ba4adf4f044e596e9d9ef30ff5cc
AirbusA321NX <be.a.linuxuser@gmail.com>
2025-06-06 14:17:45 +0530

    model.py fix

--- models.py
+++ models.py
@@ -1,32 +1,26 @@
-from sqlalchemy import Column , Integer , String
+from sqlalchemy import Column, Integer, String
 from database import Base
 
-class user(Base):
-    __tablename__= "user_detail"
+class User(Base):
+    __tablename__ = "user_detail"
 
-    id = Column(Integer ,primary_key = True , index= True)
-    name = Column(String , index = True)
-    email = Column(String ,Unique = True, index = True)
-    password = Column(String , index = True)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String, index=True)
 
+class Admin(Base):
+    __tablename__ = "admin_detail"
 
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String, index=True)
 
+class Grievances(Base):
+    __tablename__ = "Grievances"
 
-    class admin(Base):
-        __tablename__ = "admin_detail"
-
-        id = Column(Integer, primary_key=True, index=True)
-        name = Column(String, index=True)
-        email = Column(String, Unique=True, index=True)
-        password = Column(String, index=True)
-
-
-
-
-
-    class Grievances(Base):
-        __tablename__="Grievances"
-        name = Column(String , index = True)
-        id = Column(Integer , primary_key=True , index = True)
-        Grievances_content = Column(String , index = True)
-        department = Column(String , index = True)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    Grievances_content = Column(String, index=True)
+    department = Column(String, index=True)
