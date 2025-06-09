e0da8d88560aefacd69f7c96ef76e0bad127828d
AirbusA321NX <be.a.linuxuser@gmail.com>
2025-06-09 12:08:53 +0530

    enum  users

--- User/models.py
+++ User/models.py
@@ -1,11 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from database import Base
+from enum import Enum
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    department_id = Column(Integer, ForeignKey("departments.id"))
-    department = relationship("Department")
+class RoleEnum(str, Enum):
+    user = "user"
+    employee = "employee"
+    admin = "admin"
+    super_admin = "super_admin"
