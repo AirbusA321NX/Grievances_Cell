eb76f8552b5d3f14f4356bc19e5a27608c2a9f2a
AirbusA321NX <be.a.linuxuser@gmail.com>
2025-06-11 12:56:07 +0530

    Login signup  internal server error fix

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from database import engine, Base
 from Department.APIs import router as dept_router
 from User.APIs import router as user_router
 from Grievances.APIs import router as grv_router
 from Comments.APIs import router as com_router
+import auth
 import User.APIs as user_apis
 from Department import models as dept_models
 from User import models as user_models
-import auth
 
+# Create database tables
 Base.metadata.create_all(bind=engine)
+dept_models.Base.metadata.create_all(bind=engine)
+user_models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(debug=True)
+app = FastAPI()
 
+# Register routers
 app.include_router(dept_router)
 app.include_router(user_router)
 app.include_router(user_apis.router)
 app.include_router(auth.router)
 app.include_router(grv_router)
 app.include_router(com_router)
-dept_models.Base.metadata.create_all(bind=engine)
-user_models.Base.metadata.create_all(bind=engine)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
